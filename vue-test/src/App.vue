4a74f38d91e25911389ca52c24e2f94ed7e1e32e
Your Name <you@example.com>
2026-04-02 19:21:34 +0800

    vue start

---
+++ vue-test/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+const message = ref('这是我的第一个 Vue 变量')
+
+setTimeout(()=>{message.value="fuck you"}, 2000)
+
+</script>
+
+<template>
+  <h1>{{ message}}</h1>
+</template>
+
+<style scoped>
+</style>
